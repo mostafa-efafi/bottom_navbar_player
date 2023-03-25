56e9cb2995b37edd24833d4f424df3d7d40f8d55
Mostafa Efafi <mostafa.ef@gmail.com>
2023-03-25 15:55:08 +0330

    added ProgressBarState,ButtonState,SourceType

---
+++ lib/src/progress_bar_state.dart
@@ -0,0 +1,14 @@
+class ProgressBarState {
+  ProgressBarState({
+    required this.current,
+    required this.buffered,
+    required this.total,
+  });
+  final Duration current;
+  final Duration buffered;
+  final Duration total;
+}
+
+enum ButtonState { paused, playing, loading, stoped }
+
+enum SourceType { asset, file, url }
