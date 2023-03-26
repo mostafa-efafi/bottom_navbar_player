1896d8fb7ecb7001cd2261733325ac2232159073
Mostafa Efafi <mostafa.ef@gmail.com>
2023-03-26 10:01:27 +0330

    start example app

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,5 @@
-import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
 import 'package:bottom_navbar_player/bottom_navbar_player.dart';
+import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,47 +13,41 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-  final _bottomNavbarPlayerPlugin = BottomNavbarPlayer();
-
   @override
   void initState() {
     super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
-    try {
-      platformVersion =
-          await _bottomNavbarPlayerPlugin.getPlatformVersion() ?? 'Unknown platform version';
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
   }
 
   @override
   Widget build(BuildContext context) {
+    final bottomNavBarPlayer = BottomNavBarPlayer();
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Plugin example app'),
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
-        ),
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            MaterialButton(
+              onPressed: () => bottomNavBarPlayer
+                  .play('https://download.samplelib.com/mp3/sample-9s.mp3'),
+              child: const Text('play Audio 1'),
+            ),
+            MaterialButton(
+              onPressed: () => bottomNavBarPlayer
+                  .play('https://download.samplelib.com/mp3/sample-12s.mp3'),
+              child: const Text('play Audio 2'),
+            ),
+            MaterialButton(
+              onPressed: () => bottomNavBarPlayer
+                  .play('https://download.samplelib.com/mp3/sample-15s.mp3'),
+              child: const Text('play Audio 3'),
+            ),
+          ],
+        )),
+        bottomSheet: bottomNavBarPlayer.view(),
       ),
     );
   }
