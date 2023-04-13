9134c9067b9e20d4f1278f2d59c45bf55f04d1e5
Mostafa Efafi <mostafa.ef@gmail.com>
2023-04-13 14:18:34 +0330

    improved UI for VideoPlayer

--- lib/src/widgets/video_player_widget.dart
+++ lib/src/widgets/video_player_widget.dart
@@ -3,59 +3,38 @@ import 'package:bottom_navbar_player/src/progress_bar_state.dart';
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
-class VideoPlayerWidget extends StatefulWidget {
+class VideoPlayerWidget extends StatelessWidget {
   final Bloc bloc;
   final ProgressBarState progressBarState;
   const VideoPlayerWidget(
       {super.key, required this.bloc, required this.progressBarState});
 
   @override
-  State<VideoPlayerWidget> createState() => _VideoPlayerWidgetState();
-}
-
-class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
-  late GlobalKey<ScaffoldState> _stateKey;
-
-  @override
-  void initState() {
-    super.initState();
-    _stateKey = GlobalKey<ScaffoldState>();
-    widget.bloc.stateKey = _stateKey;
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey.shade900,
-      key: _stateKey,
       body: ValueListenableBuilder(
-        valueListenable: widget.bloc.buttonNotifier,
+        valueListenable: bloc.buttonNotifier,
         builder: (BuildContext _, value, Widget? __) {
           return value == ButtonState.loading
               ? const Center(child: CircularProgressIndicator())
               : Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                    widget.bloc.videoPlayerController.value.isInitialized
+                    bloc.videoPlayerController.value.isInitialized
                         ? Stack(
                             children: [
                               AspectRatio(
-                                aspectRatio: widget.bloc.videoPlayerController
-                                    .value.aspectRatio,
-                                child: VideoPlayer(
-                                    widget.bloc.videoPlayerController),
+                                aspectRatio: bloc
+                                    .videoPlayerController.value.aspectRatio,
+                                child: VideoPlayer(bloc.videoPlayerController),
                               ),
                               _closeButton(),
                             ],
                           )
                         : Container(),
-                    Align(
-                      alignment: Alignment.bottomCenter,
-                      child: Column(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [_sliderContainer(), _controllerButtons()],
-                      ),
-                    ),
+                    _sliderContainer(),
+                    _controllerButtons()
                   ],
                 );
         },
@@ -66,7 +45,7 @@ class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
   Widget _sliderContainer() {
     const textStyle = TextStyle(fontSize: 10, color: Colors.white);
     return ValueListenableBuilder<ProgressBarState>(
-        valueListenable: widget.bloc.progressNotifier,
+        valueListenable: bloc.progressNotifier,
         builder: (BuildContext _, value, Widget? __) {
           return Directionality(
             textDirection: TextDirection.ltr,
@@ -91,11 +70,11 @@ class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
                           valueIndicatorColor: Colors.grey.shade800),
                       child: Expanded(
                         child: Slider.adaptive(
-                          value: widget.bloc.sliderDoubleConvertor(
+                          value: bloc.sliderDoubleConvertor(
                               position: value.current,
                               audioDuration: value.total),
                           onChanged: (inChangeVal) =>
-                              widget.bloc.seek(value.total * inChangeVal),
+                              bloc.seek(value.total * inChangeVal),
                           thumbColor: Colors.white,
                           activeColor: Colors.red,
                           inactiveColor: Colors.grey[600],
@@ -117,112 +96,106 @@ class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
   }
 
   Widget _controllerButtons() {
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 7),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          /// [Play speed] button
-          ValueListenableBuilder<PlaySpeed>(
-            valueListenable: widget.bloc.speedNotifier,
-            builder: (_, value, __) {
-              return SizedBox(
-                width: 32,
-                height: 32,
-                child: FloatingActionButton(
-                  onPressed: () => widget.bloc.setPlayerSpeed(
-                      value == PlaySpeed.play2x
-                          ? PlaySpeed.play1x
-                          : PlaySpeed.play2x),
-                  elevation: 0,
-                  heroTag: null,
-                  mini: true,
-                  backgroundColor: Colors.white12,
-                  child: Text(
-                    value == PlaySpeed.play2x ? '1X' : '2X',
-                    style: const TextStyle(
-                        fontSize: 13, fontWeight: FontWeight.w900),
-                  ),
-                ),
-              );
-            },
-          ),
-          const SizedBox(
-            width: 10,
-          ),
-
-          /// Play button from [5 seconds ago]
-          FloatingActionButton(
-            onPressed: () => widget.bloc.moveFor5Second(isForward: false),
-            elevation: 0,
-            heroTag: null,
-            mini: true,
-            backgroundColor: Colors.white12,
-            child: const Icon(
-              Icons.replay_5_rounded,
-              color: Colors.white,
-            ),
-          ),
-          const SizedBox(
-            width: 10,
-          ),
-
-          /// [play] and [pause] button
-          ValueListenableBuilder<ButtonState>(
-            valueListenable: widget.bloc.buttonNotifier,
-            builder: (_, value, __) {
-              return FloatingActionButton(
-                backgroundColor: Colors.white12,
-                mini: true,
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        /// [Play speed] button
+        ValueListenableBuilder<PlaySpeed>(
+          valueListenable: bloc.speedNotifier,
+          builder: (_, value, __) {
+            return SizedBox(
+              width: 32,
+              height: 32,
+              child: FloatingActionButton(
+                onPressed: () => bloc.setPlayerSpeed(value == PlaySpeed.play2x
+                    ? PlaySpeed.play1x
+                    : PlaySpeed.play2x),
                 elevation: 0,
                 heroTag: null,
-                onPressed: () => value == ButtonState.playing
-                    ? widget.bloc.pause()
-                    : widget.bloc.startPlaying() /* onPressPlayButton(value) */,
-                child: playButtonChildGeneratior(value),
-              );
-            },
-          ),
-          const SizedBox(
-            width: 10,
-          ),
+                mini: true,
+                backgroundColor: Colors.white12,
+                child: Text(
+                  value == PlaySpeed.play2x ? '1X' : '2X',
+                  style: const TextStyle(
+                      fontSize: 13, fontWeight: FontWeight.w900),
+                ),
+              ),
+            );
+          },
+        ),
+        const SizedBox(
+          width: 10,
+        ),
 
-          /// Play button from the [next 5 seconds]
-          FloatingActionButton(
-            onPressed: () => widget.bloc.moveFor5Second(isForward: true),
+        /// Play button from [5 seconds ago]
+        FloatingActionButton(
+          onPressed: () => bloc.moveFor5Second(isForward: false),
+          elevation: 0,
+          heroTag: null,
+          mini: true,
+          backgroundColor: Colors.white12,
+          child: const Icon(
+            Icons.replay_5_rounded,
+            color: Colors.white,
+          ),
+        ),
+        const SizedBox(
+          width: 10,
+        ),
+
+        /// [play] and [pause] button
+        ValueListenableBuilder<ButtonState>(
+          valueListenable: bloc.buttonNotifier,
+          builder: (_, value, __) {
+            return FloatingActionButton(
+              backgroundColor: Colors.white12,
+              mini: true,
+              elevation: 0,
+              heroTag: null,
+              onPressed: onPressPlayButton(value),
+              child: playButtonChildGeneratior(value),
+            );
+          },
+        ),
+        const SizedBox(
+          width: 10,
+        ),
+
+        /// Play button from the [next 5 seconds]
+        FloatingActionButton(
+          onPressed: () => bloc.moveFor5Second(isForward: true),
+          elevation: 0,
+          heroTag: null,
+          mini: true,
+          backgroundColor: Colors.white12,
+          child: const Icon(
+            Icons.forward_5_rounded,
+            color: Colors.white,
+          ),
+        ),
+        const SizedBox(
+          width: 10,
+        ),
+
+        /// [stop] button
+        SizedBox(
+          width: 32,
+          height: 32,
+          child: FloatingActionButton(
+            onPressed: () => bloc.stop(),
             elevation: 0,
             heroTag: null,
             mini: true,
             backgroundColor: Colors.white12,
             child: const Icon(
-              Icons.forward_5_rounded,
+              Icons.stop_rounded,
+              size: 20,
               color: Colors.white,
             ),
           ),
-          const SizedBox(
-            width: 10,
-          ),
-
-          /// [stop] button
-          SizedBox(
-            width: 32,
-            height: 32,
-            child: FloatingActionButton(
-              onPressed: () => widget.bloc.stop(),
-              elevation: 0,
-              heroTag: null,
-              mini: true,
-              backgroundColor: Colors.white12,
-              child: const Icon(
-                Icons.stop_rounded,
-                size: 20,
-                color: Colors.white,
-              ),
-            ),
-          ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 
@@ -235,7 +208,7 @@ class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
           width: 30,
           child: FloatingActionButton(
             onPressed: () {
-              widget.bloc.stop();
+              bloc.stop();
             },
             backgroundColor: Colors.black12,
             elevation: 0,
@@ -256,6 +229,7 @@ class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
     return list.first;
   }
 
+  /// generate icon for [play],[pause] button
   Widget playButtonChildGeneratior(ButtonState state) {
     switch (state) {
       case ButtonState.loading:
@@ -269,16 +243,17 @@ class _VideoPlayerWidgetState extends State<VideoPlayerWidget> {
     }
   }
 
+  /// for [play],[pause] onPress button
   void Function() onPressPlayButton(ButtonState state) {
     switch (state) {
       case ButtonState.loading:
-        return widget.bloc.startPlaying;
+        return bloc.startPlaying;
       case ButtonState.stoped:
-        return widget.bloc.startPlaying;
+        return bloc.startPlaying;
       case ButtonState.paused:
-        return widget.bloc.startPlaying;
+        return bloc.pause;
       case ButtonState.playing:
-        return widget.bloc.pause;
+        return bloc.pause;
     }
   }
 }
