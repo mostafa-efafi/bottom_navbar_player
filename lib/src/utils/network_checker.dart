025c530ea3ca043e0ebb51b590329413da3a1e8d
Mostafa Efafi <mostafa.ef@gmail.com>
2023-04-16 21:33:49 +0330

    added more comment for BottomNavBarPlayer

--- lib/src/utils/network_checker.dart
+++ lib/src/utils/network_checker.dart
@@ -4,6 +4,7 @@ class NetworkChecker {
   static const urlTest = 'google.com';
 
   /// Return [true] if the device was connected to the Internet, otherwise [false]
+  /// It is sent to [Google] [Ping] and the result is returned
   Future<bool> checkConnection() async {
     try {
       final result = await InternetAddress.lookup(urlTest);
