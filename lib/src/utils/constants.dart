c8150894054462b78669e310c3ec646c6468bd2c
Mostafa Efafi <mostafa.ef@gmail.com>
2023-05-03 20:40:41 +0330

    added Constants class

---
+++ lib/src/utils/constants.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: non_constant_identifier_names, constant_identifier_names
+import 'package:flutter/material.dart';
+
+class Constants {
+  /// [Colors]
+  /// background color fot video player and audio player
+  static final PLAYER_BACKGROUND_COLOR = Colors.grey.shade900;
+  static const SLIDER_OVERLAY_COLER = Colors.red;
+  static final SLIDER_VALUE_INDICATORCOLOR = Colors.grey.shade800;
+  static const SLIDER_THUMB_COLOR = Colors.white;
+  static final SLIDER_INACTIVE_COLOR = Colors.grey.shade600;
+
+  /// [Style]
+  static const SLIDER_TEXT_STYLE = TextStyle(fontSize: 10, color: Colors.white);
+  static const CONTROLLER_BUTTON_TEXT_STYLE =
+      TextStyle(fontSize: 13, fontWeight: FontWeight.w900);
+}
